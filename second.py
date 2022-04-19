e52b8912ca16d6c47250362267288510b348282a
hujie20200707 <20170510hj@gmail.com>
2022-04-19 12:15:52 +0800

    add second file

---
+++ second.py
@@ -0,0 +1,12 @@
+#name = input("please enter an name:")
+#print("hello,",name)
+s1 = 72;
+s2 = 85;
+r = (s2-s1)/s1*100;
+print('小明成绩提高百分点: %.5f %%' %r)
+student = ['Michale','Bob','Tracy']
+print(student[:])
+student.append('adam')
+print(student[:])
+student.insert(1,'Jack')
+print(student[:])
